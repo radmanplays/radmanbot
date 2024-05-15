391a8c0147e27a740409b693d22cdb3a8b549017
radmanplays <95340057+radmanplays@users.noreply.github.com>
2024-05-15 07:09:36 +0000

    push

--- index.js
+++ index.js
@@ -3,7 +3,10 @@ const readline = require('readline');
 var tpsPlugin = require('mineflayer-tps')(mineflayer)
 const { mineflayer: mineflayerViewer } = require('prismarine-viewer');
 const { pathfinder, Movements, goals } = require('mineflayer-pathfinder');
+const { GoalBlock } = require('mineflayer-pathfinder').goals;
 
+let isLookingAtPlayer = false;
+let isMoving = false;
 
 const options = {
     host: 'sus.shhnowisnottheti.me', //Change this to the ip you want.
@@ -15,57 +18,74 @@ const options = {
 const bot = mineflayer.createBot(options);
 
 bot.loadPlugin(tpsPlugin)
+bot.loadPlugin(pathfinder);
 
 function welcome() {
     bot.chat('/server creative');
+    const viewer = mineflayerViewer(bot, { port: 3000 });
+    const viewer2 = mineflayerViewer(bot, { port: 3001, firstPerson: true });
     bot.chat('hello, im radmanbot. a friendly bot designed for this server by radmanplays.');
     bot.chat('type !help in chat to get started');
-    const viewer = mineflayerViewer(bot, { port: 3000 });
 }
 
 bot.once('spawn', welcome);
 
 function chat(message, username) {
+    if (message == "Discord"){
+      var username = username.split('» ')[1];
+    }
+    console.log(message + " > " + username);
     if(username == "!ping"){
       if(bot.player.ping != -1){
-        bot.chat("Pong! " + "UNKNOWN PING!")
+        bot.chat("Pong! " + "0")
       } else {
         bot.chat("Pong! " + bot.player.ping)
       }
     }
     if(username == "!tps"){
       if(bot.getTps() != -1){
-        bot.chat("servertps: " + "UNKNOWN")
+        bot.chat("servertps: " + "20")
       } else {
         bot.chat("server tps: " + bot.getTps())
       }
     }
-    if(username == "!help"){
-      bot.chat("commands: !help, !tps, !ping, !stalk, !goto")
+    if (username === "!help") {
+        bot.chat("commands: !help, !tps, !ping, !stalk, !goto, !stopgoingto, !look, !stoplook");
     }
     if(username == "!stalk"){
       bot.chat("you can stalk me here: https://shiny-space-funicular-74gq57w55vwfr66v-3000.app.github.dev/")
+      bot.chat("first person: https://shiny-space-funicular-74gq57w55vwfr66v-3001.app.github.dev/")
     }
     if(username == ("!goto") ){
       bot.chat("usage: !goto <x> <y> <z>")
     }
-    if(username.startsWith("!goto ") ){
-      var targetLength = "!goto".length;
-      var message = username.toString().trim();
-      if ( //If
-          message.length > targetLength && //The length of the message is longer than the target length
-          message.substring(0, targetLength).trim().toLowerCase() === "!goto" //And, the content from character 0 (first) to that of the length, trimmed and put to lowercase is "!goto"
-        ) {
-          var x = message.substring(targetLength+1);
-          var y = message.substring(targetLength+3);
-          var z = message.substring(targetLength+5);
-          if(x & y & z){
-            bot.pathfinder.setGoal(new GoalBlock(x, y, z))
-            bot.chat("going to " + x + " " + y + " " + z + " ")
-          }
+    if (username.startsWith("!goto ")) {
+      const coordinates = username.substring("!goto ".length).split(' ').map(coord => parseFloat(coord));
+      if (coordinates.length === 3 && coordinates.every(coord => !isNaN(coord))) {
+          const [x, y, z] = coordinates;
+          bot.chat(`Going to ${x}, ${y}, ${z}`);
+          bot.pathfinder.setGoal(new GoalBlock(x, y, z));
+          isMoving = true;
+      } else {
+          bot.chat("Invalid coordinates. Usage: !goto <x> <y> <z>");
       }
+  }
+  if (username === "!stopgoingto") {
+    if (isMoving) {
+        bot.pathfinder.stop(); // Stop the bot from moving
+        bot.chat("Stopped moving.");
+        isMoving = false; // Set isMoving to false after stopping
+    } else {
+        bot.chat("Bot is not currently moving.");
     }
-    console.log(message + " > " + username);
+  }
+  if (username === "!look") {
+    startLookAtPlayer();
+    bot.chat("Started looking at nearest player.");
+  } else if (username === "!stoplook") {
+      stopLookAtPlayer();
+      bot.chat("Stopped looking at nearest player.");
+  }
 }
 
 bot.on('chat', chat);
@@ -107,17 +127,28 @@ function end(reason) {
 
 bot.on('end', end)
 
-function lookAtNearestPlayer () {
-  const playerFilter = (entity) => entity.type === 'player'
-  const playerEntity = bot.nearestEntity(playerFilter)
-  
-  if (!playerEntity) return
-  
-  const pos = playerEntity.position.offset(0, playerEntity.height, 0)
-  bot.lookAt(pos)
+function startLookAtPlayer() {
+  isLookingAtPlayer = true;
 }
 
-bot.on('physicTick', lookAtNearestPlayer)
+function stopLookAtPlayer() {
+  isLookingAtPlayer = false;
+}
+
+function lookAtNearestPlayer() {
+  if (isLookingAtPlayer) {
+      const playerFilter = (entity) => entity.type === 'player';
+      const playerEntity = bot.nearestEntity(playerFilter);
+
+      if (playerEntity) {
+          const pos = playerEntity.position.offset(0, playerEntity.height, 0);
+          bot.lookAt(pos);
+      }
+  }
+}
+
+// Call lookAtNearestPlayer in your bot's main loop or wherever appropriate
+bot.on('physicTick', lookAtNearestPlayer);
 
 
 
